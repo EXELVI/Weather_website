117e757bc729b92d14b0fe2e9cf95d52cc7ccdc1
EXELVI <exelvi@outlook.com>
2024-06-28 10:07:31 +0200

    chore: Add logic to fetch and update daily quote

--- index.js
+++ index.js
@@ -14,10 +14,36 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', (req, res) => {
+
+
+app.get('/', async (req, res) => {
+    let quote = JSON.parse(await fs.readFileSync("quote.json", "utf8"))
+    //If is more than 10 hours since the last update (quote.updated)
+    if (Date.now() - quote.updated > 36000000) {
+        //BearerAuth  (http, Bearer)
+        let response = await axios.get('https://quotes.rest/qod', {
+            headers: {
+                Authorization: `Bearer ${process.env.theySaidSoApi}`
+            }
+
+        }).catch(err => console.log(err.response.data));
+        if (!response?.data) {
+            console.log("Error getting the quote");
+            return res.render('index', { quote: quote, cod: "Error getting the quote", data: "" });
+        }
+
+        quote = {
+            quote: response.data.contents.quotes[0],
+            updated: Date.now()
+        }
+
+        fs.writeFileSync("quote.json", JSON.stringify(quote, null, 3));
+    }
+
+
+    res.render('index', { quote: quote });
 
 });
-
 http.createServer(app).listen(portHttp, () => {
     console.log(`Server is running on port ${portHttp}`);
 })
